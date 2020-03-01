5f7337091580ff949a3d2cfa03e79a51050fe5a9
Berat Vıcıl <beratvicil@gmail.com>
2020-03-02 01:35:23 +0300

    02.03.2020

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 var active =  false;
+var pointdrag = false;
 nodeCount = 0;  
 
 
@@ -9,25 +10,33 @@ document.addEventListener("mousemove", move, false);
 document.addEventListener("mousedown", moveStart, false);
 
 
+
 //--------------------------------------------------
 //Path Çiz
 //function pathAdd()
-
 {
     var a = document.getElementById("a");
     var b = document.getElementById("b");
-    var svg = document.createElement("svg");
-    svg.setAttribute("width","400px");
-    svg.setAttribute("height","400px");
+    var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+    svg.setAttribute("width","800px");
+    svg.setAttribute("height","600px");
+    svg.style.position = "absolute";
+    svg.style.top=0;
+    svg.style.left=0;
+    svg.style.zIndex=-1;
     document.getElementById("container").appendChild(svg);
-
-    var newPath = document.createElement("path");
+    
+    
+    var newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    /*
     var ax = a.getBoundingClientRect().left;
     var ay = a.getBoundingClientRect().top;
     var bx = b.getBoundingClientRect().left;
     var by = b.getBoundingClientRect().top;
 
     var coords = "M" + ax + "," + ay + " " + bx + "," + by;
+    */
+    var coords = "M200,300 2,21";
     newPath.setAttribute("d",coords);
     newPath.setAttribute("fill","none");
     newPath.setAttribute("stroke","black");
@@ -57,6 +66,14 @@ function nodeEkle()
     delBtn.setAttribute("onclick",btnId);
     delBtn.setAttribute("class","delBtn")
     newNodeHeader.appendChild(delBtn);
+
+    //Output point
+    var opoint = document.createElement("div");
+    //var opointId = "";
+    opoint.setAttribute("class","connection output");
+    opoint.setAttribute("id","output"+nodeCount+","+0);
+    newNode.appendChild(opoint);
+
     
 
     nodeCount++;
@@ -76,13 +93,27 @@ function nodeSil(del)
 
 
 //------------------------------------------------------
-//Node sürükle
+//Mouse down event
 function moveStart(e)
 {
+    //Node hareket ettir
     if (e.target.classList.contains("nodeheader")) //(sadece headera tıklandıysa hareket ettir)
     {
-            node=e.target.parentNode ;//Hangi iteme tıklandı id değiş 
-            active = true;//Sürükleme switchi aç
+        node=e.target.parentNode ;//Hangi iteme tıklandı id değiş 
+        active = true;//Sürükleme switchi aç
+    }
+
+    //Path hareket ettir
+    if (e.target.classList.contains("output"))
+    {
+        point = e.target; //Hangi outputa tıklandı
+        pointdrag = true;
+
+        newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+        svg.appendChild(newPath);
+        newPath.setAttribute("fill","none");
+        newPath.setAttribute("stroke","black");
+        newPath.setAttribute("stroke-width","3");
     }
 
 
@@ -90,16 +121,30 @@ function moveStart(e)
 
 function move(e)
 {
+    //Node sürükle
     if (active)
     {
         node.style.left = e.x - (node.offsetWidth/2) + "px";
         node.style.top = e.y - 16 + "px"; 
     }
+
+    //Point bağla (path oluştur)
+    if (pointdrag)
+    {
+        var rect = point.getBoundingClientRect();
+        var l = rect.left+7;
+        var t = rect.top+7;
+        var coords = "M"+l+","+ t +" "+ e.x + ","+ e.y;
+        newPath.setAttribute("d",coords);
+    }
+
     
 }
 
 function moveEnd()
 {
     active = false;
+    pointdrag = false;
+    point = 0;
     node = 0;
 }
