0f4dc7c4be70550fce6977e5e18ee1c9bede4380
Berat Vıcıl <beratvicil@gmail.com>
2020-03-07 18:58:53 +0300

    2020-03-07

--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 var active =  false;
 var pointdrag = false;
 
-//sayaçlar
-nodeCount = 0; 
+
 pathCount = 0;  
-pointCount = 0;
 
 //arraylar
 let nodes = []; 
@@ -36,29 +34,35 @@ function Node(_id)
     var delBtn = document.createElement("div");
     var btnId = "nodeSil('"+_id+"')";
     delBtn.setAttribute("onclick",btnId);
-    delBtn.setAttribute("class","delBtn")
+    delBtn.setAttribute("class","delBtn");
     newNodeHeader.appendChild(delBtn);
 
-    //Output pointi
-    points[pointCount] = new Point("point" + pointCount,"output", _id);
+    //Output point instance
+    //empty indise yada yeni indise point ekle
+    for (let i = 0; i <= points.length; i++) 
+    {
+        if (points[i] == undefined || points[i]._id == "empty") 
+        {
+            points[i] = new Point("point" + i, "output", _id);
+            console.log(i+ ". indise yeni point obje koyuldu");
 
-    //Output point
-    let opoint = document.createElement("div");
-    opoint.setAttribute("class","connection output");
-    opoint.setAttribute("id","point" + pointCount );
-    newNode.appendChild(opoint);
+            //Output point element
+            let opoint = document.createElement("div");
+            opoint.setAttribute("class","connection output");
+            opoint.setAttribute("id","point" + i );
+            newNode.appendChild(opoint);
+            break;
+        }
+    }
 
-    //Arttır
-    nodeCount++;
-    pointCount++;
 }
 
 //Path objesi tanımla
-function Path(_id, parent_node_id, target_node_id)
+function Path(_id, parent_point_id, target_point_id)
 {
     this._id = _id;
-    this.parent_node_id = parent_node_id;
-    this.target_node_id = target_node_id;
+    this.parent_point_id = parent_point_id;
+    this.target_point_id = target_point_id;
 }
 
 //Point objesi tanımla
@@ -68,8 +72,6 @@ function Point(_id, type, parent_node_id)
     this.type = type;
     this.parent_node_id = parent_node_id;
 }
-
-
 
 //--------------------------------------------------
 //SVG Oluştur
@@ -89,8 +91,17 @@ function Point(_id, type, parent_node_id)
 //Node ekle
 function nodeEkle()
 {
-    //nodeCount id'si ile node objesi oluştur
-    nodes[nodeCount] = new Node("node" + nodeCount); 
+    //empty indise yada yeni indise node ekle
+    for (let i = 0; i <= nodes.length; i++) 
+    {
+        if (nodes[i] == undefined || nodes[i]._id == "empty") 
+        {
+            nodes[i] = new Node("node" + i);
+            console.log(i+ ". indise yeni node obje koyuldu");
+            break;
+        }
+    }
+
 }
 
 
@@ -105,35 +116,40 @@ function nodeSil(del)
     {
         if (nodes[i]._id == del)
         {
-            nodes[i] = undefined;
+            nodes[i]._id = "empty";
             break;
         }
         
     }
 
-    //Parenti silinecek node olan pointleri bul ve verilerini sil
+
+    //Parenti silinecek node olan pointleri bul ve pathlarını, verilerini sil
     for (let i = 0; i < points.length; i++) 
-    {
+    { 
+        //Bu nodeun pointi bul
         if (points[i].parent_node_id == del)
         {
-            points[i] = undefined;
-            pointCount--;
+            //Point'e ait pathları sil
+            //var all_paths = document.getElementsByClassName(delNode.getAttribute("id"));
+            for(let j = 0; j < paths.length; j++)
+            {
+                if (paths[j].parent_point_id == points[i]._id)
+                {
+                    path_to_del = document.getElementById(paths[j]._id);
+                    path_to_del.remove();
+                    paths[j] = new Path("empty", "empty", "empty"); //verileri sil
+                }
+            }
+
+            points[i] = new Point("empty", "empty", "empty");
+            break;
         }
     }
 
-    var all_paths = document.getElementsByClassName(delNode.getAttribute("id"));
-
-    while(all_paths[0]) 
-    {
-        all_paths[0].remove();
-        pathCount-=1;
-    }
     delNode.remove();
     
-    nodeCount--;
     
 }
-
 
 //------------------------------------------------------
 //Mouse down event
@@ -146,7 +162,8 @@ function moveStart(e)
         e = e || window.event;
         e.preventDefault();
 
-        node=e.target.parentNode ;//Hangi iteme tıklandı id değiş 
+        node=e.target.parentNode ;//Hangi iteme tıklandı id al
+        node_id = node.getAttribute("id");
         active = true;//Sürükleme switchi aç
         
     }
@@ -168,17 +185,18 @@ function moveStart(e)
         {
             if ( points[i].type == "output" && points[i]._id == point.getAttribute("id") )   
             {
-                //Pointin parentini bul
-                parent_cache =  points[i].parent_node_id;
+                //Pointin id cachele
+                parent_cache =  points[i]._id;
                 
                 for (let j = 0; j < paths.length; j++) 
                 {
                     //Pointin parentine ait path varmı kontrol et
-                    if (paths[j].parent_node_id == parent_cache)
+                    if (paths[j].parent_point_id == parent_cache)
                     {
                         //Path eklemeyi kapat
                         //? ilerde eski pathı silip yeni path ekle 
                         pointdrag = false;
+                        console.log("Zaten bir path var");
                     }
                 }
             }
@@ -188,17 +206,24 @@ function moveStart(e)
         //Path ekle
         if (pointdrag)
         {
-            paths[pathCount] = new Path("path"+ pathCount, parent_cache, undefined);
+            for (let i = 0; i <= paths.length; i++) 
+            {
+                if (paths[i] == undefined || paths[i]._id == "empty") 
+                {
+                    paths[i] = new Path("path"+ i, parent_cache, undefined);
+                    console.log(i+ ". indise yeni path obje koyuldu");
 
-            newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-            newPath.setAttribute("id","path" + pathCount);
-            //newPath.setAttribute("parent_node_id",e.target.parentNode.getAttribute("id"));
-            newPath.classList.add(e.target.parentNode.getAttribute("id"));
-            svg.appendChild(newPath);
-            newPath.setAttribute("fill","none");
-            newPath.setAttribute("stroke","black");
-            newPath.setAttribute("stroke-width","3");
-            pathCount++;
+                    //Output path element
+                    newPath = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+                    newPath.setAttribute("id","path" + i);
+                    svg.appendChild(newPath);
+                    newPath.setAttribute("fill","none");
+                    newPath.setAttribute("stroke","black");
+                    newPath.setAttribute("stroke-width","3");
+                    break;
+                }
+            }
+            
         }
     }
 
@@ -213,44 +238,55 @@ function move(e)
         node.style.left = e.x - (node.offsetWidth/2) + "px";
         node.style.top = e.y - 16 + "px"; 
 
-        //Pathları sürükle
-        var all_paths = document.getElementsByClassName(node.getAttribute("id"));
+        
+        console.log(paths);
+        console.log(points);
+        console.log(nodes);
 
-        //Tüm pathları döngüde güncelle
-        for(let i=0; i < all_paths.length ; i++)
+        //Pathları güncelle
+        //Bu nodeun pointlerini bul
+        for (let i = 0; i < points.length; i++) 
         {
-            let path_points = all_paths[i].getAttribute("d"); //Nokta koordinatları
-            let all_points= path_points.split(" ");    //Başlangıç koordinatı  
-            let connections = node.getElementsByClassName("connection"); //Connection obje
+            if ( points[i].type == "output" && points[i].parent_node_id == node_id )   
+            {
+                point_cache =  points[i]._id;
 
-            let rect  = connections[0].getBoundingClientRect();
-            let startx = rect.left+7;
-            let starty = rect.top+7;
-            let endx = all_points[2].split(",")[0];
-            let endy = all_points[2].split(",")[1];
-            let curvex = Math.abs( (endx-startx)/2 );
-            if (curvex<50)
-            { curvex = 50; }
+                //Bulunan pointe bağlı pathları bul
+                for(let j=0; j<paths.length; j++)
+                {
+                    //Bulunan pathı güncelle
+                    if ( paths[j].parent_point_id == point_cache )
+                    {
+                        let update_path = document.getElementById(paths[j]._id);
+                        let all_points = update_path.getAttribute("d").split(" ");
 
-            let coords = "M" +startx + "," + starty 
-            +" C"+ (startx+curvex) + "," + starty
-            +","+ (endx-curvex) + "," + endy   
-            +" "+ endx + "," + endy;
+                        let rect = document.getElementById(point_cache).getBoundingClientRect();
+                        let startx = rect.left+7;
+                        let starty = rect.top+7;
+                        let endx = all_points[2].split(",")[0];
+                        let endy = all_points[2].split(",")[1];
+                        let curvex = Math.abs( (endx-startx)/2 );
+                        if (curvex<50)
+                        { curvex = 50; }
 
-            all_paths[i].setAttribute("d",coords)
+                        let coords = "M" +startx + "," + starty 
+                        +" C"+ (startx+curvex) + "," + starty
+                        +","+ (endx-curvex) + "," + endy   
+                        +" "+ endx + "," + endy;
+
+                        update_path.setAttribute("d",coords);
+
+                    }
+                }
+            }
         }
     }
+
+
 
     //Point bağla (path oluştur)
     if (pointdrag)
     {
-        /*
-        var rect = point.getBoundingClientRect();
-        var l = rect.left+7;
-        var t = rect.top+7;
-        var coords = "M"+l+","+ t +" "+ e.x + ","+ e.y;
-        */
-
         let rect  = point.getBoundingClientRect();
         let startx = rect.left+7;
         let starty = rect.top+7;
@@ -277,4 +313,5 @@ function moveEnd()
     pointdrag = false;
     point = 0;
     node = 0;
+    
 }
